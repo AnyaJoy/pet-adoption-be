dfeebcd9032c3f0dda7c77b694302a18e80aefd0
Anya Joy <urda-a@mail.ru>
2022-01-03 21:41:31 +0200

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { postgrator } = require("./lib/db");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3006;
 require("dotenv").config();
 const petsRoute = require("./routes/pets");
 const usersRoute = require("./routes/users");
@@ -13,16 +13,16 @@ app.use(cors());
 app.use("/pets", petsRoute);
 app.use("/users", usersRoute);
 
-// app.get("/", (req, res) => {
-//   res.send('Hello to Pet-Adoption API')
-// });
+app.get("/", (req, res) => {
+  res.send('Hello to Pet-Adoption API')
+});
 
 postgrator
   .migrate()
   .then((result) => {
     console.log(`migrated db successfully:`, result);
-    app.listen(port, () => {
-      console.log(`Running on port ${port}`);
+    app.listen(PORT, () => {
+      console.log(`Running on port ${PORT}`);
     });
   })
   .catch((error) => console.error(error));
