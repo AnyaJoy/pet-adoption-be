2d4247ea5a5bbc85637513e5de4bfa66d182e2e0
Anya Joy <urda-a@mail.ru>
2022-01-04 23:53:05 +0200

    Final push for delivering

--- middleware/addUserStatus.js
+++ middleware/addUserStatus.js
@@ -1,7 +1,7 @@
 async function addUserStatus(req, res, next) {
   req.body.type = "User";
   req.body.bio = "Change your bio...";
-  req.body.picture = "";
+  req.body.picture = "https://res.cloudinary.com/deaqcrhah/image/upload/v1641327982/default-profile-pic_fku9i9.png";
   next();
 }
 
